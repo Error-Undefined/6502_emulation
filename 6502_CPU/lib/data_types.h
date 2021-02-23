c4face8f2abb9fd3069ef87090a2af9772a9bde0
Error-Undefined <ottome@live.com>
2021-02-23 10:48:52 +0100

    Add u32 data type

--- 6502_CPU/lib/data_types.h
+++ 6502_CPU/lib/data_types.h
@@ -1,6 +1,9 @@
 
-// Word and Byte data types
+//u32 and s32
 typedef signed int s32;
+typedef unsigned int u32;
+
+// Word and Byte data types
 typedef unsigned int LongWord;
 typedef unsigned short Word;
 typedef unsigned char Byte;
