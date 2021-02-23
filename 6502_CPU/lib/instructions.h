85034d1505571aca7754b21e6c83593428eed67a
Error-Undefined <ottome@live.com>
2021-02-23 12:32:45 +0100

    Add all LDA instructions

--- 6502_CPU/lib/instructions.h
+++ 6502_CPU/lib/instructions.h
@@ -9,7 +9,13 @@ enum Instruction
 {
   // LDA
   INS_LDA_IM = 0xA9,
-  INS_LDA_ZP = 0xA5
+  INS_LDA_ZP = 0xA5,
+  INS_LDA_ZP_X = 0xB5,
+  INS_LDA_ABS = 0xAD,
+  INS_LDA_ABS_X = 0xBD,
+  INS_LDA_ABS_Y = 0xB9,
+  INS_LDA_IND_X = 0xA1,
+  INS_LDA_IND_Y = 0xB1
 };
 
 #endif
