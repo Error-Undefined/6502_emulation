8f61f4778214f7149f73589a7fc8eebd03d93e8c
Error-Undefined <ottome@live.com>
2021-02-23 12:25:37 +0100

    Small fix

--- 6502_CPU/test/test_lda.h
+++ 6502_CPU/test/test_lda.h
@@ -1,12 +1,6 @@
 #include "include_headers.h"
 
-static struct cpu_struct* cpu;
-static struct memory_struct* memory;
-
-static void before();
-
-static char* test_lda_im();
-
-static char* all_lda_test();
+struct cpu_struct* cpu;
+struct memory_struct* memory;
 
 int run_all_lda_test(struct cpu_struct* cpu_in, struct memory_struct* memory_in);
