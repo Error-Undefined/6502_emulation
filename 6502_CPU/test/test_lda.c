9b1c32fe57da7ec64e709ba9daaebfd19bb2468a
Error-Undefined <ottome@live.com>
2021-02-23 12:50:13 +0100

    Test for LDA_ZP

--- 6502_CPU/test/test_lda.c
+++ 6502_CPU/test/test_lda.c
@@ -9,6 +9,28 @@ static void before()
   reset_cpu(cpu, memory);
 }
 
+static void test_lda_im_setup(Byte value_to_load, s32* cycles)
+{
+  // Using
+  memory->memory_array[0xFFFC] = INS_LDA_IM;
+  memory->memory_array[0xFFFD] = value_to_load;
+
+  // Execute
+  execute(cpu, cycles);
+}
+
+static void test_lda_zp_setup(Instruction_Type instruction, Byte jump_to_zeropage, Byte value_in_zeropage, s32* cycles)
+{
+  //Using
+  memory->memory_array[0xFFFC] = instruction;
+  memory->memory_array[0xFFFD] = jump_to_zeropage;
+  memory->memory_array[jump_to_zeropage] = value_in_zeropage;
+
+  execute(cpu, cycles);
+}
+
+// LDA_IM
+
 static char* test_nothing_happens()
 {
   //Using
@@ -24,14 +46,10 @@ static char* test_nothing_happens()
 
 static char* test_lda_im()
 {
-  //Using
-  memory->memory_array[0xFFFC] = INS_LDA_IM;
-  memory->memory_array[0xFFFD] = 0x50;
+  //Setup and run
   s32 cycles = 2;
-
-  //Execute the cpu
-  execute(cpu, &cycles);
-
+  test_lda_im_setup(0x50, &cycles);
+  
   // Assert
   mu_assert("Accumulator loaded wrong value", cpu->Acc == 0x50);
   mu_assert("LDA_IM did not consume exactly 2 cycles", cycles == 0);
@@ -40,13 +58,9 @@ static char* test_lda_im()
 
 static char* test_lda_im_zeroflag()
 {
-  //Using
-  memory->memory_array[0xFFFC] = INS_LDA_IM;
-  memory->memory_array[0xFFFD] = 0x0;
+  //Setup and run
   s32 cycles = 2;
-
-  //Execute the cpu
-  execute(cpu, &cycles);
+  test_lda_im_setup(0x0, &cycles);
 
   // Assert
   mu_assert("Accumulator loaded wrong value", cpu->Acc == 0x0);
@@ -57,13 +71,9 @@ static char* test_lda_im_zeroflag()
 
 static char* test_lda_im_negativeflag()
 {
-  //Using
-  memory->memory_array[0xFFFC] = INS_LDA_IM;
-  memory->memory_array[0xFFFD] = 0b1000000; //Bit 7 set
+  //Setup and run
   s32 cycles = 2;
-
-  //Execute the cpu
-  execute(cpu, &cycles);
+  test_lda_im_setup(0b1000000, &cycles);
 
   // Assert
   mu_assert("Accumulator loaded wrong value", cpu->Acc == 0b1000000);
@@ -71,6 +81,19 @@ static char* test_lda_im_negativeflag()
   mu_assert("LDA_IM did not set the negative flag", cpu->status_flags.N == 1);
   return 0;
 }
+
+static char* test_lda_zp()
+{
+  //Setup and run
+  s32 cycles = 3;
+  Byte value = 0x20;
+  test_lda_zp_setup(INS_LDA_ZP, 0x10, value, &cycles);
+
+  mu_assert("Accumulator loaded wrong value", cpu->Acc == value);
+  mu_assert("LDA_ZP did not consume exactly 3 cycles", cycles == 0);
+  return 0;
+}
+
 
 static char* all_lda_test()
 {
@@ -85,6 +108,9 @@ static char* all_lda_test()
 
   before();
   mu_run_test(test_lda_im_negativeflag);
+
+  before();
+  mu_run_test(test_lda_zp);
 
   return 0;
 }
