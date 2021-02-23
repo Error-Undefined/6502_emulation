fad59683dd4a94537d9f408ba1f115f4e8ce9e2e
Error-Undefined <ottome@live.com>
2021-02-23 15:39:36 +0100

    Test for LDX_ABS

--- 6502_CPU/test/test_ldx.c
+++ 6502_CPU/test/test_ldx.c
@@ -10,8 +10,28 @@ static void before()
   reset_cpu(cpu, memory);
 }
 
+static char* test_ldx_im()
+{
+  //Given
+  Byte value = 0x3f;
+  s32 cycles = 2;
+  memory->memory_array[0xFFFC] = INS_LDX_ABS;
+  memory->memory_array[0xFFFD] = value;
+  
+  //Run
+  execute(cpu, &cycles);
+
+  //Expect
+  mu_assert("X register was not loaded with the right value", cpu->X == value);
+  mu_assert("Wrong number of cycles consumed", cycles == 0);
+  return 0;
+}
+
 static char* all_ldx_test()
 {
+  before();
+  mu_run_test(test_ldx_im);
+
   return 0;
 }
 
